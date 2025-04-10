5e24c8c4948e8b34d14fcff56837b7cf2705fd74
Jose Villarroel <163937499+villarroelsaad@users.noreply.github.com>
2025-04-10 18:04:37 -0400

    first

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { NabBar } from "./components/NavBar";
+import { DarkMode } from "./components/DarkMode";
+import { Outlet } from "react-router-dom";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <main className="transition-all">
+        <div>
+      <DarkMode />
+          <NabBar />
+        </div>
+    <section className="flex justify-center mt-5 sm:ml-40 sm:p-0 p-4">
+   
+
+        <Outlet/>
+
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/hooks/useDarkMode.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+export function useDarkMode() { // Correcta exportacion.
+  const [darkMode, setDarkMode] = useState(() => {
+    const storedDarkMode = localStorage.getItem('darkMode');
+    return storedDarkMode === 'true';
+  });
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', darkMode);
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return [darkMode, toggleDarkMode];
+}
